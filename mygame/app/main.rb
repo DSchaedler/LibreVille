b23f4fe86ed0375cdfb339e9536af138860de5f2
Dee Schaedler <deeschaedler@gmail.com>
2025-08-16 16:37:57 -0400

    Add icons. Implement cartesian to be extra

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -30,6 +30,8 @@ end
 def logic args
   # This is where you would put any game logic that needs to run every frame.
 
+  args.state.rotation ||= -45   # (Integer Angle) Isometric View Angle
+
   # Create an array of arrays, for a grid.
   grid_size = 10
   args.state.matrix ||= grid_size.map { |i| 
@@ -62,7 +64,6 @@ def render_matrix args
   args.state.sprite_scale     = 1   # (Float %)       Shrink or grow the sprites
   args.state.iso_scale        = 0.5   # (Float %)       Adjust vertical spacing to account for isometric distortion
   args.state.sprite_grouping  = 8     # (Integer)       Account for extra pixels on the sides of sprites
-  args.state.rotation         = -45   # (Integer Angle) Isometric View Angle
 
   # TODO Hardcode these values - Grab some reference numbers from the kenney assets
   args.state.spritebox ||= args.gtk.calcspritebox "sprites/kenney/isometric_tiles_base/landscape_tiles_067.png"
@@ -86,7 +87,7 @@ def render_matrix args
         reference_y = ((columns - 1) * args.state.sprite_spacing)
         
         # Convert the reference coordinates to isometric coordinates.
-        x_isometric, y_isometric = to_isometric(reference_x, reference_y)
+        x_isometric, y_isometric = to_isometric(reference_x, reference_y, args.state.rotation)
 
         # The sprite path is stored in the matrix, so we have to get it from there.
         path = args.state.matrix[rows-1][columns-1]
@@ -95,9 +96,17 @@ def render_matrix args
         # TODO: Make all sprites the same height, so we can hardcode this value.
         height = args.gtk.calcspritebox(path).y
 
+        if args.state.rotation == 0
+          x_alignment = reference_x * ( 0.75 + args.state.iso_scale )
+          y_alignment = reference_y * (args.state.sprite_scale / ( 1+ args.state.iso_scale ))
+        else
+          x_alignment = x_isometric
+          y_alignment = (y_isometric * args.state.iso_scale ) + (720 / 2) - (args.state.sprite_height / 5)
+        end
+
         args.outputs.sprites << { 
-          x: x_isometric, 
-          y: (y_isometric * args.state.iso_scale ) + (720 / 2) - (args.state.sprite_height / 5), # We have to adjust the Y value to account for the isometric distortion. Adding 720 / 2 centers the isometric view vertically.
+          x: x_alignment, 
+          y: y_alignment, # We have to adjust the Y value to account for the isometric distortion. Adding 720 / 2 centers the isometric view vertically.
           anchor_x: 0.5, # Center the sprite horizontally.
           anchor_y: 0, # Anchor the sprite to the bottom, so sprites of different heights remain grid aligned.
           w: args.state.horizontal_scale,
@@ -111,21 +120,67 @@ def render_matrix args
 end
 
 def user_interface args
-  icon_zoom_in  = "sprites/kenney/game_icons/png/white/2x/zoom_in.png"
-  icon_zoom_out = "sprites/kenney/game_icons/png/white/2x/zoom_out.png"
-  icon_menu     = "sprites/kenney/game_icons/png/white/2x/bars_horizontal.png"
-  grid_menu     = "sprites/kenney/game_icons/png/white/2x/menu_grid.png"
+  icon_zoom_in      = "sprites/kenney/game_icons/png/white/2x/zoom_in.png"
+  icon_zoom_out         = "sprites/kenney/game_icons/png/white/2x/zoom_out.png"
+  icon_menu             = "sprites/kenney/game_icons/png/white/2x/bars_horizontal.png"
+  icon_grid_menu        = "sprites/kenney/game_icons/png/white/2x/menu_grid.png"
+  icon_rotate_isometric = "sprites/kenney/game_icons_expansion/png/white/2x/device_tilt_right.png"
+  icon_rotate_cartesian = "sprites/kenney/game_icons_expansion/png/white/2x/device_tilt_left.png"
 
   args.state.menu_button_rect       ||= args.layout.rect(row: 0,                                col: args.layout.col_max_index, w: 1, h: 1)
   args.state.zoom_in_button_rect    ||= args.layout.rect(row: (args.layout.row_max_index - 1),  col: args.layout.col_max_index, w: 1, h: 1)
   args.state.zoom_out_button_rect   ||= args.layout.rect(row: args.layout.row_max_index,        col: args.layout.col_max_index, w: 1, h: 1)
   args.state.grid_menu_button_rect  ||= args.layout.rect(row: 0,                                col: 0,                         w: 1, h: 1)
+  args.state.rotate_button_rect     ||= args.layout.rect(row: args.layout.row_max_index,        col: 0,                         w: 1, h: 1)
 
   args.outputs.sprites << args.state.menu_button_rect.merge(path: icon_menu)
   args.outputs.sprites << args.state.zoom_in_button_rect.merge(path: icon_zoom_in)
   args.outputs.sprites << args.state.zoom_out_button_rect.merge(path: icon_zoom_out)
-  args.outputs.sprites << args.state.grid_menu_button_rect.merge(path: grid_menu)
+  args.outputs.sprites << args.state.grid_menu_button_rect.merge(path: icon_grid_menu)
 
+  if args.state.rotation == -45
+    args.outputs.sprites << args.state.rotate_button_rect.merge(path: icon_rotate_cartesian)
+  else
+    args.outputs.sprites << args.state.rotate_button_rect.merge(path: icon_rotate_isometric)
+  end
+
+  if args.inputs.mouse.up
+    if args.state.menu_button_rect.intersect_rect? args.inputs.mouse
+      args.state.show_menu ||= false
+      args.state.show_menu = !args.state.show_menu
+    end
+
+    if args.state.zoom_in_button_rect.intersect_rect? args.inputs.mouse
+      # TODO: Implement Render Targets to allow zooming in and out.
+    end
+
+    if args.state.zoom_out_button_rect.intersect_rect? args.inputs.mouse
+      # TODO: Implement Render Targets to allow zooming in and out.
+    end
+
+    if args.state.grid_menu_button_rect.intersect_rect? args.inputs.mouse
+      # TODO: Implement grid menu for selecting view.
+    end
+
+    if args.state.rotate_button_rect.intersect_rect? args.inputs.mouse
+      puts "clicked rotate"
+      if args.state.rotation == -45
+        args.state.rotation = 0
+      else
+        args.state.rotation = -45
+      end
+    end
+  end
+
+end
+
+def tile_menu args
+  icon_residential  = "sprites/kenney/game_icons/png/white/2x/home.png"
+  icon_commercial   = "sprites/kenney/game_icons/png/white/2x/shopping_cart.png"
+  icon_industrial   = "sprites/kenney/game_icons/png/white/2x/wrench.png"
+  icon_road         = "sprites/kenney/game_icons/png/white/2x/bars_vertical.png"
+  icon_nothing      = "sprites/kenney/game_icons/png/white/2x/cross.png"
+  icon_attraction   = "sprites/kenney/game_icons/png/white/2x/star.png"
 end
 
 def debug_interface args
@@ -147,7 +202,7 @@ def tick args
   logic args
 
   args.outputs.background_color = [29, 32, 43]
-  
+
   render_matrix args
   user_interface args
   debug_interface args
